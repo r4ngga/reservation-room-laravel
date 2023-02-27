07ac47ff92318ef30f824a21d27658388064aca1
r4ngga <ranggajack99@gmail.com>
2023-02-27 20:44:33 +0700

    reroute class

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,45 +18,45 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', 'AuthController@index');
-Route::get('/login', 'AuthController@loginpage');
-Route::post('/login', 'AuthController@login');
+Route::get('/', [AuthController::class, 'index']);
+Route::get('/login', [AuthController::class, 'loginpage']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/register', 'AuthController@register');
-Route::post('/register', 'UserController@store');
+Route::get('/register', [AuthController::class, 'register']);
+Route::post('/register', [UserController::class, 'store']);
 
 Route::group(['middleware' => ['auth']], function () {
 
-    Route::get('/logout', 'AuthController@logout')->middleware('auth');
-    Route::get('/myaccount', 'AuthController@myaccount')->middleware('auth');
-    Route::get('/setting', 'AuthController@settingaccount')->middleware('auth');
-    Route::post('/setting', 'AuthController@updatesettingacc')->middleware('auth');
-    Route::get('/changepassword', 'AuthController@changepassword')->middleware('auth');
-    Route::post('/changepassword', 'AuthController@updatechangepassword')->middleware('auth');
+    Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth');
+    Route::get('/myaccount', [AuthController::class, '@myaccount'])->middleware('auth');
+    Route::get('/setting', [AuthController::class, 'settingaccount'])->middleware('auth');
+    Route::post('/setting', [AuthController::class, 'updatesettingacc'])->middleware('auth');
+    Route::get('/changepassword', [AuthController::class, 'changepassword'])->middleware('auth');
+    Route::post('/changepassword', [AuthController::class, 'updatechangepassword'])->middleware('auth');
     Route::group(['middleware' => ['cek_login:1']], function () {
-        Route::get('/admindashboard', 'AdminController@admindashboard');
-        Route::get('/rooms', 'RoomController@show_all');
-        Route::get('/rooms/addroom', 'RoomController@insert');
-        Route::post('/rooms', 'RoomController@store');
-        Route::get('/change/{room}', 'RoomController@edit');
-        Route::patch('/rooms/{room}', 'RoomController@update');
-        Route::delete('/rooms/{room}', 'RoomController@destroy');
+        Route::get('/admindashboard', [AdminController::class, 'admindashboard']);
+        Route::get('/rooms', [RoomController::class, 'show_all']);
+        Route::get('/rooms/addroom', [RoomController::class, 'insert']);
+        Route::post('/rooms', [RoomController::class, 'store']);
+        Route::get('/change/{room}', [RoomController::class, 'edit']);
+        Route::patch('/rooms/{room}', [RoomController::class, 'update']);
+        Route::delete('/rooms/{room}', [RoomController::class, 'destroy']);
 
-        Route::get('/reservation', 'ReservationController@confirmationbooking');
-        Route::post('/confirmpaymentroom', 'ReservationController@confirmpaymentreservation');
-        Route::get('/users', 'UserController@show_all_user');
+        Route::get('/reservation', [ReservationController::class, 'confirmationbooking']);
+        Route::post('/confirmpaymentroom', [ReservationController::class, 'confirmpaymentreservation']);
+        Route::get('/users', [UserController::class, 'show_all_user']);
     });
 
     Route::group(['middleware' => ['cek_login:2']], function () {
-        Route::get('/userdashboard', 'ReservationController@reservationlist');
-        Route::get('/userdashboard/{reservation}', 'ReservationController@paidreservation');
-        Route::post('/paymentroom', 'ReservationController@paymentreservation');
+        Route::get('/userdashboard', [ReservationController::class, 'reservationlist']);
+        Route::get('/userdashboard/{reservation}', [ReservationController::class, 'paidreservation']);
+        Route::post('/paymentroom', [ReservationController::class, 'paymentreservation']);
 
-        Route::get('/roomsdashboard', 'ReservationController@index');
-        Route::post('/roomsdashboard', 'ReservationController@filter');
-        Route::get('/bookingrooms/{room}', 'ReservationController@reservation');
-        Route::post('/bookingrooms', 'ReservationController@booking');
+        Route::get('/roomsdashboard', [ReservationController::class, 'index']);
+        Route::post('/roomsdashboard', [ReservationController::class, 'filter']);
+        Route::get('/bookingrooms/{room}', [ReservationController::class, 'reservation']);
+        Route::post('/bookingrooms', [ReservationController::class, 'booking']);
 
-        Route::get('/history', 'ReservationController@loghistory');
+        Route::get('/history', [ReservationController::class, 'loghistory']);
     });
 });
